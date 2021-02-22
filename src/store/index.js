81883f3e8db52196a36ae9fe5a8b85d4b2b59365
aidanpirish <aidanpirish@gmail.com>
2021-02-21 21:29:23 -0500

    added functionality to 'all' and 'click' upgrades

--- src/store/index.js
+++ src/store/index.js
@@ -11,7 +11,7 @@ export default new Vuex.Store({
   state: {
     statusInterval: 0.5,
     clickMultiplier: 1,
-    tacoCount: 999,
+    tacoCount: 100000,
     tacosPerInterval: 0,
     tacoStore: [
       { name: 'chef', cost: 100, tacosPerInterval: 1, owned: 0, nextCostFactor: 0.125, upgrades: 0 },
@@ -19,17 +19,17 @@ export default new Vuex.Store({
       { name: 'truck', cost: 1000, tacosPerInterval: 20, owned: 0, nextCostFactor: 0.125, upgrades: 0 }
     ],
     tacoUpgrades: [
-      { name: 'click', cost: 100, owned: 0 },
-      { name: 'chef', cost: 500, owned: 0 },
-      { name: 'cart', cost: 750, owned: 0 },
-      { name: 'truck', cost: 1000, owned: 0 },
-      { name: 'all', cost: 10000, owned: 0 }
+      { name: 'click', cost: 100, nextCostFactor: 0.5 },
+      { name: 'chef', cost: 500, nextCostFactor: 0.5 },
+      { name: 'cart', cost: 750, nextCostFactor: 0.5 },
+      { name: 'truck', cost: 1000, nextCostFactor: 0.5 },
+      { name: 'all', cost: 10000, nextCostFactor: 0.5 }
     ],
     countAfterPurchase: 0
   },
   mutations: {
     incrementTacoCount (state) {
-      state.tacoCount++
+      state.tacoCount = state.tacoCount + state.clickMultiplier
     },
     addTacos (state) {
       var tacosFromCapital = 0
@@ -62,9 +62,12 @@ export default new Vuex.Store({
       if (state.countAfterPurchase >= 0) {
         // if all, increase all intervals
         if (purchaseName === 'all') {
-
-        } else if (purchaseName === 'clicks') {
-
+          state.tacoStore.forEach(store => {
+            store.upgrades++
+            store.tacosPerInterval = store.tacosPerInterval * 2
+          })
+        } else if (purchaseName === 'click') {
+          state.clickMultiplier = (state.clickMultiplier * 2)
         } else {
           var storeObj = state.tacoStore.find(obj => {
             return obj.name === purchaseName
@@ -73,6 +76,7 @@ export default new Vuex.Store({
           storeObj.tacosPerInterval = storeObj.tacosPerInterval * 2
         }
         state.tacoCount = state.countAfterPurchase
+        upgradeObj.cost = Math.round(upgradeObj.cost + (upgradeObj.cost * upgradeObj.nextCostFactor))
       } else {
         console.log('insufficient funds, would be left with: ' + state.countAfterPurchase)
       }
